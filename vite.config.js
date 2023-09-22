cb98f4198406ec4b860c210dbf7c7f94385c1a2f
Thomas Pfau <thomas.pfau@aalto.fi>
2023-09-22 15:07:00 +0300

    Updating dev frontend to use https

--- vite.config.js
+++ vite.config.js
@@ -1,93 +1,99 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "@/",
-        replacement: fileURLToPath(new URL("./src/", import.meta.url)),
-      },
-    ],
-  },
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "^/exp/.*/.*/.+": {
-        target: "https://localhost:8081",
-        rewrite: (path) => path.replace(/^\/exp/, "/run"),
-        configure: (proxy, _options) => {
-          proxy.on("error", (err, _req, _res) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req, _res) => {
-            console.log(
-              "Sending Request:",
-              req.method,
-              req.url,
-              " => TO THE TARGET =>  ",
-              proxyReq.method,
-              proxyReq.protocol,
-              proxyReq.host,
-              proxyReq.path,
-              JSON.stringify(proxyReq.getHeaders())
-            );
-          });
-          proxy.on("proxyRes", (proxyRes, req, _res) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url,
-              JSON.stringify(proxyRes.headers)
-            );
-          });
+export default  ({mode})  => { 
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};  
+  return defineConfig({
+    resolve: {
+      alias: [
+        {
+          find: "@/",
+          replacement: fileURLToPath(new URL("./src/", import.meta.url)),
         },
-        secure: false,
-      },
-      "^/api/.*": {
-        target: "https://localhost:8081",
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-      },
-      "^/pilot/.*/.*/.+": {
-        target: "https://localhost:8081",
-        rewrite: (path) =>
-          path.replace(/^\/pilot(\/\w+\/\w+)(\/.*)$/, "/task$1/execute$2"),
-        secure: false,
-      },
-      "^/editing/.*/.*/.+": {
-        target: "https://localhost:8081",
-        rewrite: (path) =>
-          path.replace(/^\/editing(\/\w+\/\w+)(\/.*)$/, "/task$1/execute$2"),
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on("error", (err, _req, _res) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req, _res) => {
-            console.log(
-              "Sending Request:",
-              req.method,
-              req.url,
-              " => TO THE TARGET =>  ",
-              proxyReq.method,
-              proxyReq.protocol,
-              proxyReq.host,
-              proxyReq.path,
-              JSON.stringify(proxyReq.getHeaders())
-            );
-          });
-          proxy.on("proxyRes", (proxyRes, req, _res) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url,
-              JSON.stringify(proxyRes.headers)
-            );
-          });
+      ],
+    },
+    https: true,
+    plugins: [vue(), basicSsl()],
+    server: {
+      proxy: {
+        "^/exp/.*/.*/.+": {
+          target: `https://${process.env.VITE_BACKENDDOMAIN}:${process.env.VITE_BACKENDPORT}`,
+          rewrite: (path) => path.replace(/^\/exp/, "/run"),
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log(
+                "Sending Request:",
+                req.method,
+                req.url,
+                " => TO THE TARGET =>  ",
+                proxyReq.method,
+                proxyReq.protocol,
+                proxyReq.host,
+                proxyReq.path,
+                JSON.stringify(proxyReq.getHeaders())
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url,
+                JSON.stringify(proxyRes.headers)
+              );
+            });
+          },
+          secure: false,
+        },
+        "^/api/.*": {
+          target: `https://${process.env.VITE_BACKENDDOMAIN}:${process.env.VITE_BACKENDPORT}`,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          secure: false,
+        },
+        "^/pilot/.*/.*/.+": {
+          target: `https://${process.env.VITE_BACKENDDOMAIN}:${process.env.VITE_BACKENDPORT}`,
+          rewrite: (path) =>
+            path.replace(/^\/pilot(\/\w+\/\w+)(\/.*)$/, "/task$1/execute$2"),
+          secure: false,
+        },
+        "^/editing/.*/.*/.+": {
+          target: `https://${process.env.VITE_BACKENDDOMAIN}:${process.env.VITE_BACKENDPORT}`,
+          rewrite: (path) =>
+            path.replace(/^\/editing(\/\w+\/\w+)(\/.*)$/, "/task$1/execute$2"),
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log(
+                "Sending Request:",
+                req.method,
+                req.url,
+                " => TO THE TARGET =>  ",
+                proxyReq.method,
+                proxyReq.protocol,
+                proxyReq.host,
+                proxyReq.path,
+                JSON.stringify(proxyReq.getHeaders())
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url,
+                JSON.stringify(proxyRes.headers)
+              );
+            });
+          },
         },
       },
     },
-  },
-});
+  });
+}
