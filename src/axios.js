d542d30aa6f9a609d935ef000887888c3398e33b
Thomas Pfau <github@thomaspfau.de>
2023-10-12 12:14:15 +0300

    Updating API fetches

--- src/axios.js
+++ src/axios.js
@@ -4,13 +4,13 @@ export function setupAxios(env) {
   axios.defaults.withCredentials = true;
 
   const SERVER_URL = `${env.VITE_BACKENDDOMAIN}${
-    env.VITE_BACKENDPORT != undefined ? ":" + env.VITE_BACKENDPORT : ""
+    env.VITE_FRONTENDPORT != undefined ? ":" + env.VITE_FRONTENDPORT : ""
   }`;
 
   axios.defaults.baseURL = env.VITE_BACKENDDOMAIN
     ? `${env.VITE_HTTPS === "true" ? "https://" : "http://"}${SERVER_URL}${
         env.VITE_BASE_URL ? env.VITE_BASE_URL : ""
-      }`
+      }/api`
     : "https://localhost:8081";
 
   // This probably needs to change and rather the certificates need to be set appropriately.
